a8a3d50e352c11a8e08f20da4d275d6bda2c2fda
EitanKats <eitan.kats96@gmail.com>
2022-03-22 15:53:55 +0200

    added additional tests

--- Test.cpp
+++ Test.cpp
@@ -3,10 +3,15 @@
 //
 #include "doctest.h"
 #include "Notebook.hpp"
+#include "Direction.hpp"
+#include "string"
 
+using std::string;
 using namespace ariel;
 
 Notebook notebook;
+const int NON_PRINTABLE_LOWER_BOUND = 32;
+const int ASCII_END = 255;
 
 TEST_CASE ("Bad input - writing empty line saved character") {
             CHECK_THROWS(notebook.write(0, 1, 1, Direction::Horizontal, "_"));
@@ -24,6 +29,28 @@ TEST_CASE ("Bad input - end of string character") {
             CHECK_THROWS(notebook.write(0, 1, 1, Direction::Vertical, "\0"));
 }
 
+TEST_CASE ("Bad input - end of line characters") {
+            CHECK_THROWS(notebook.write(0, 1, 1, Direction::Horizontal, "\n"));
+            CHECK_THROWS(notebook.write(0, 1, 1, Direction::Vertical, "\n"));
+
+            CHECK_THROWS(notebook.write(0, 1, 1, Direction::Horizontal, "\r"));
+            CHECK_THROWS(notebook.write(0, 1, 1, Direction::Vertical, "\r"));
+}
+
+TEST_CASE ("Bad input - Non printable characters in text") {
+    string testText = "abc";
+    //ascii 32 is space so it is valid
+    for (int i = 0; i < NON_PRINTABLE_LOWER_BOUND; ++i) {
+        char invalidChar = (char) i;
+                CHECK_THROWS(notebook.write(3, 0, 0, Direction::Horizontal, testText + invalidChar));
+    }
+
+    for (int i = 127; i < ASCII_END; ++i) {
+        char invalidChar = (char) i;
+                CHECK_THROWS(notebook.write(3, 0, 0, Direction::Horizontal, testText + invalidChar));
+    }
+}
+
 TEST_CASE ("Attempt to double write on the same spot") {
     notebook.write(0, 1, 1, Direction::Horizontal, "a");
             CHECK_THROWS(notebook.write(0, 1, 1, Direction::Horizontal, "1"));
@@ -33,4 +60,12 @@ TEST_CASE ("Attempt to write on deleted text") {
     notebook.write(0, 1, 1, Direction::Horizontal, "a");
     notebook.erase(0, 1, 1, Direction::Horizontal, 1);
             CHECK_THROWS(notebook.write(0, 1, 1, Direction::Horizontal, "a"));
+}
+
+TEST_CASE ("write and read tests") {
+    string firstText = "Hello world";
+    notebook.write(1, 0, 0, Direction::Horizontal, "Hello world");
+    string textFromNotebook = notebook.read(1, 0, 0, Direction::Horizontal, 11);
+            CHECK(firstText == textFromNotebook);
+
 }
